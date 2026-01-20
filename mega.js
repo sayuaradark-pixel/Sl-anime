3ea351365149f1405752803fb80f0901ddc63147
sayuaradark-pixel <sayuaradark@gmail.com>
2026-01-20 11:42:37 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "sayuaradark@gmail.com",
-  password: "Sayura2008***7",
+  password: "Sayura2008***8",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
